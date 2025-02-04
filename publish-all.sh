866d63b517e620b66341760104543557f7c801f7
David Komer <6406986+dakom@users.noreply.github.com>
2025-02-04 20:58:05 +0200

    version

--- publish-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd packages/layer-climb-config && cargo publish
-cd packages/layer-climb-proto && cargo publish
-cd packages/layer-climb-address && cargo publish
-cd packages/layer-climb-core && cargo publish
-cd packages/layer-climb && cargo publish
-cd packages/layer-climb-cli && cargo publish
